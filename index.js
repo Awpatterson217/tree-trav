5f72a456424819391302ed270181c61d61a96176
Adam Patterson <awpatterson217@gmail.com>
2017-09-02 00:35:10 -0500

    Patch 1.0.1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**                                             2017
  * @fileOverview A library to handle nested directory structures in Node.js using the EventEmitter API and recursion.
  * @author <a href="mailto:awpatterson217@gmail.com">Adam Patterson</a>
- * @version 1.0.0
+ * @version 1.0.1
  *                              Tested on Windows, Node.js v8.
  */
 
